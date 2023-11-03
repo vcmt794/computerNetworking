6868080f311676c450197b2af973fa912eac9e17
vcmt794 <128240027+vcmt794@users.noreply.github.com>
2023-11-03 15:33:48 +0700

    Update Screenshot.java

--- Screenshot.java
+++ Screenshot.java
@@ -6,19 +6,27 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class Screenshot {
+    Robot robot;
+    Rectangle rectangle;
+    String filename;
     public Screenshot(String fileName){//fileName/filePath: if u insert a path, it's will save as that.
         try{
-            Robot robot = new Robot();
-            Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-            BufferedImage bufferedImage = robot.createScreenCapture(rectangle);
-            ImageIO.write(bufferedImage,"png",new File(fileName));
-            System.out.println("Succesfully, save as " + fileName);
-        }catch (Exception e){
-            System.out.println("Can't take a screenshot due to error: "+e.toString());
-        }
+            robot = new Robot();
+            rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            filename = fileName;
+        }catch (Exception e){ System.out.println("Can't take a screenshot due to error: "+e.toString());}
+    }
+
+    public void Capture(){
+        try{
+            BufferedImage bufferedImage = this.robot.createScreenCapture(rectangle);
+            ImageIO.write(bufferedImage,"png",new File(this.filename));
+            System.out.println("Succesfully, save as " + this.filename);
+        }catch (Exception e){ System.out.println("Can't take a screenshot due to error: "+e.toString());}
     }
 
     public static void main(String[] arg){
-        new Screenshot("img2.png");
+        Screenshot screenshot = new Screenshot("img3.png");
+        screenshot.Capture();
     }
 }
